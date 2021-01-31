3435de11716b4580fc6200e5a6a8c4b4c8c9a6e5
Hassan Alizadeh <alizadeh118@gmail.com>
2021-02-01 02:03:10 +0330

    Add error page and remove unused files

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-row align-h="center" align-v="center" class="vh-100">
+    <b-col cols="auto" class="text-center">
+      <img src="/404.jpg" alt="not found">
+      <h1 class="my-4">
+        Page not found
+      </h1>
+      <NuxtLink to="/">
+        Go back to the homepage
+      </NuxtLink>
+    </b-col>
+  </b-row>
+</template>
